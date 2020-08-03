82e7202d73938237f3bac41c820f31caf9b6adb8
Ho Sy Tuan Anh <tuananh.0706999@gmail.com>
2020-08-04 04:40:00 +0700

    Add validate in Create

--- HelloWord/Controller/user.controller.js
+++ HelloWord/Controller/user.controller.js
@@ -34,6 +34,23 @@ module.exports.get= function(req,res){
 
 module.exports.postCreate = function(req, res){
     req.body.id= shortid.generate();
+    var err=[];
+    if(!req.body.name){
+        err.push('Name is required');
+
+    }
+    if(!req.body.phone){
+        err.push('Phone is required');
+
+    }
+    if(err.length)
+    {
+        res.render('user/create', {
+            err: err,
+            value: req.body
+        })
+        return;
+    }
     db.get('user').push(req.body).write();
     res.redirect('/user');
 };
